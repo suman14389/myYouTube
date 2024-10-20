9989b04a825d27a9c56e32164a2c618c296075a4
Suman Naik Guguloth <suman.guguloth@vwo.com>
2024-10-20 21:29:43 +0530

    remove the hardcoded comment config list

--- mytube/src/configs/commentsConfigList.js
+++ mytube/src/configs/commentsConfigList.js
@@ -1,165 +1,165 @@
 import { getRandomNames } from "../Utils/helper";
 
-export const commentsConfigList = [
-    {
-        name:  getRandomNames(),
-        comment: 'This is a comment',
-        date: '2021-01-01',
-        replies: [
-            {
-                name: getRandomNames(), 
-                comment: 'This is a reply',
-                date: '2021-01-01',
-                replies: [
-                    {
+// export const commentsConfigList = [
+//     {
+//         name:  getRandomNames(),
+//         comment: 'This is a comment',
+//         date: '2021-01-01',
+//         replies: [
+//             {
+//                 name: getRandomNames(), 
+//                 comment: 'This is a reply',
+//                 date: '2021-01-01',
+//                 replies: [
+//                     {
                        
-                        name: getRandomNames(),
-                        comment: 'This is a reply',
-                        date: '2021-01-01',
-                        replies: []
-                    },
-                    {
+//                         name: getRandomNames(),
+//                         comment: 'This is a reply',
+//                         date: '2021-01-01',
+//                         replies: []
+//                     },
+//                     {
                        
-                        name: getRandomNames(),
-                        comment: 'This is a reply',
-                        date: '2021-01-01',
-                        replies: []
-                    }
-                ]
-            }
-        ]
-    },
-    {
+//                         name: getRandomNames(),
+//                         comment: 'This is a reply',
+//                         date: '2021-01-01',
+//                         replies: []
+//                     }
+//                 ]
+//             }
+//         ]
+//     },
+//     {
        
-        name: 'Jane Doe',
-        comment: 'This is a comment',
-        date: '2021-01-01',
-        replies: [
-            {
+//         name: 'Jane Doe',
+//         comment: 'This is a comment',
+//         date: '2021-01-01',
+//         replies: [
+//             {
                 
-                name: getRandomNames(),
-                comment: 'This is a reply',
-                date: '2021-01-01',
-                replies: [{
+//                 name: getRandomNames(),
+//                 comment: 'This is a reply',
+//                 date: '2021-01-01',
+//                 replies: [{
                     
-                    name: getRandomNames(),
-                    comment: 'This is a reply',
-                    date: '2021-01-01',
-                    replies: [
-                        {
-                            name: getRandomNames(),
-                            comment: 'This is a reply',
-                            date: '2021-01-01',
-                            replies: [
-                                {
-                                    name: getRandomNames(),
-                                    comment: 'This is a reply',
-                                    date: '2021-01-01',
-                                    replies: [
-                                        {
-                                            name: getRandomNames(),
-                                            comment: 'This is a reply',
-                                            date: '2021-01-01',
-                                            replies: [
-                                                {
-                                                    name: getRandomNames(),
-                                                    comment: 'This is a reply',
-                                                    date: '2021-01-01',
-                                                    replies: []
-                                                }
-                                            ]
-                                        },
-                                        {
+//                     name: getRandomNames(),
+//                     comment: 'This is a reply',
+//                     date: '2021-01-01',
+//                     replies: [
+//                         {
+//                             name: getRandomNames(),
+//                             comment: 'This is a reply',
+//                             date: '2021-01-01',
+//                             replies: [
+//                                 {
+//                                     name: getRandomNames(),
+//                                     comment: 'This is a reply',
+//                                     date: '2021-01-01',
+//                                     replies: [
+//                                         {
+//                                             name: getRandomNames(),
+//                                             comment: 'This is a reply',
+//                                             date: '2021-01-01',
+//                                             replies: [
+//                                                 {
+//                                                     name: getRandomNames(),
+//                                                     comment: 'This is a reply',
+//                                                     date: '2021-01-01',
+//                                                     replies: []
+//                                                 }
+//                                             ]
+//                                         },
+//                                         {
                        
-                                            name: getRandomNames(),
-                                            comment: 'This is a reply',
-                                            date: '2021-01-01',
-                                            replies: []
-                                        }
-                                    ]
-                                }
-                            ]
-                        }
-                    ]
-                }]
-            },
-            {
+//                                             name: getRandomNames(),
+//                                             comment: 'This is a reply',
+//                                             date: '2021-01-01',
+//                                             replies: []
+//                                         }
+//                                     ]
+//                                 }
+//                             ]
+//                         }
+//                     ]
+//                 }]
+//             },
+//             {
                        
-                name: getRandomNames(),
-                comment: 'This is a reply',
-                date: '2021-01-01',
-                replies: []
-            }
-        ]
-    },
-    {
-        name: getRandomNames(),
-        comment: 'This is a comment',
-        date: '2021-01-01',
-        replies: []
-    },
-    {
-        name: getRandomNames(),
-        comment: 'This is a comment',
-        date: '2021-01-01',
-        replies: [
-            {
-                name: getRandomNames(),
-                comment: 'This is a reply',
-                date: '2021-01-01',
-                replies: []
-            },
-            {
+//                 name: getRandomNames(),
+//                 comment: 'This is a reply',
+//                 date: '2021-01-01',
+//                 replies: []
+//             }
+//         ]
+//     },
+//     {
+//         name: getRandomNames(),
+//         comment: 'This is a comment',
+//         date: '2021-01-01',
+//         replies: []
+//     },
+//     {
+//         name: getRandomNames(),
+//         comment: 'This is a comment',
+//         date: '2021-01-01',
+//         replies: [
+//             {
+//                 name: getRandomNames(),
+//                 comment: 'This is a reply',
+//                 date: '2021-01-01',
+//                 replies: []
+//             },
+//             {
                        
-                name: getRandomNames(),
-                comment: 'This is a reply',
-                date: '2021-01-01',
-                replies: []
-            }
-        ]
-    },
-    {
-        name: getRandomNames(),
-        comment: 'This is a comment',
-        date: '2021-01-01',
-        replies: [
-            {
+//                 name: getRandomNames(),
+//                 comment: 'This is a reply',
+//                 date: '2021-01-01',
+//                 replies: []
+//             }
+//         ]
+//     },
+//     {
+//         name: getRandomNames(),
+//         comment: 'This is a comment',
+//         date: '2021-01-01',
+//         replies: [
+//             {
                        
-                name: getRandomNames(),
-                comment: 'This is a reply',
-                date: '2021-01-01',
-                replies: []
-            },
-            {
+//                 name: getRandomNames(),
+//                 comment: 'This is a reply',
+//                 date: '2021-01-01',
+//                 replies: []
+//             },
+//             {
                        
-                name: getRandomNames(),
-                comment: 'This is a reply',
-                date: '2021-01-01',
-                replies: []
-            },
-            {
+//                 name: getRandomNames(),
+//                 comment: 'This is a reply',
+//                 date: '2021-01-01',
+//                 replies: []
+//             },
+//             {
                        
-                name: getRandomNames(),
-                comment: 'This is a reply',
-                date: '2021-01-01',
-                replies: []
-            }
-        ]
-    },
-    {
-        name: getRandomNames(),
-        comment: 'This is a comment',
-        date: '2021-01-01',
-        replies: []
-    },
-    {
-        name: getRandomNames(),
-        comment: 'This is a comment',
-        date: '2021-01-01',
-        replies: []
-    },
+//                 name: getRandomNames(),
+//                 comment: 'This is a reply',
+//                 date: '2021-01-01',
+//                 replies: []
+//             }
+//         ]
+//     },
+//     {
+//         name: getRandomNames(),
+//         comment: 'This is a comment',
+//         date: '2021-01-01',
+//         replies: []
+//     },
+//     {
+//         name: getRandomNames(),
+//         comment: 'This is a comment',
+//         date: '2021-01-01',
+//         replies: []
+//     },
 
-]
+// ]
 
-
+export const commentsConfigList= [];
 
